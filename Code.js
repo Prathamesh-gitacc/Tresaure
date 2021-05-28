d5a118e0f54a561d169c1df9f59d215fc0e2a051
Prathamesh-gitacc <76098843+Prathamesh-gitacc@users.noreply.github.com>
2021-05-28 11:32:38 +0530

    Add files via upload

---
+++ Code.js
@@ -0,0 +1,23 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+
+    fill("white")
+    text("C U T N I F O N", 700,50);
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 700,70)
+
+    fill("white")
+    text("A T A D A S B A", 400,250);
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 400,270);
+
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Project43
+TREASURE HUNT
